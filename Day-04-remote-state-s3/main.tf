0826a33830ef488e7656101a4d4b0fc961ef2db7
Goutham Kumar Aare <gouthamreddy671@gmail.com>
2025-04-25 13:00:43 +0530

    enabled versioning

--- Day-04-remote-state-s3/main.tf
+++ Day-04-remote-state-s3/main.tf
@@ -1,4 +1,13 @@
 resource "aws_s3_bucket" "remote-state-s3" {
     bucket = "agk-statefile-remote"
+    
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+    bucket = aws_s3_bucket.remote-state-s3.id
+
+    versioning_configuration {
+      status = "Enabled"
+    }
   
 }
