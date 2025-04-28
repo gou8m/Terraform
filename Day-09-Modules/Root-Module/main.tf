831309250ff981ef3bd169cfb83cb03e583110e9
Goutham Kumar Aare <gouthamreddy671@gmail.com>
2025-04-28 20:49:03 +0530

    modified main file

--- Day-09-Modules/Root-Module/main.tf
+++ Day-09-Modules/Root-Module/main.tf
@@ -11,3 +11,21 @@ module "s3" {
     
   
 }
+
+
+module "rds" {
+  source = "../child-module/rds"
+
+  providers = {
+    aws.primary = aws.primary
+    aws.replica = aws.replica
+  }
+
+  primary_identifier  = "mydb-primary"
+  replica_identifier  = "mydb-replica"
+  instance_class      = "db.t3.micro"
+  allocated_storage   = 20
+  username            = "admin"
+  password            = "Cloud123"
+  db_name             = "mydatabase"
+}
